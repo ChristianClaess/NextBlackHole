7ed6a0821168635efc93c591820bc1ece43c7ba2
Christian <Christian_Claessen@web.de>
2026-02-17 13:54:39 +0100

    modify config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
-
+const repo = "NextBlackHole";
 const nextConfig: NextConfig = {
   output: "export",
-  /* config options here */
-
   trailingSlash: true,
   images: { unoptimized: true },
-
-  // needed for GitHub Pages project sites: https://username.github.io/repo/
   basePath: `/${repo}`,
   assetPrefix: `/${repo}/`,
+
 };
 
 export default nextConfig;
