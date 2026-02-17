9370c6eece69d2c1f9f2a4c4097530c40a998b09
Christian <Christian_Claessen@web.de>
2026-02-17 13:51:42 +0100

    modify config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
+
+  trailingSlash: true,
+  images: { unoptimized: true },
+
+  // needed for GitHub Pages project sites: https://username.github.io/repo/
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
