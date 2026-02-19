910159ad41e491829dd8f8246ac87712915db97c
Christian <Christian_Claessen@web.de>
2026-02-19 18:33:38 +0100

    repairs path

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
-
+const repo = "NextBlackHole";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 
 };
 
