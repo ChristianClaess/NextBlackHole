007a9bface5dd8aeda11fa8fb45f06f28ab8774f
Christian <Christian_Claessen@web.de>
2026-02-19 19:04:59 +0100

    changes basepath

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
-const repo = "NextBlackHole";
+//const repo = "NextBlackHole"; //outdated
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: process.env.PAGES_BASE_PATH, //`/${repo}`,
+  assetPrefix: process.env.PAGES_BASE_PATH//`/${repo}/`,
 
 };
 
